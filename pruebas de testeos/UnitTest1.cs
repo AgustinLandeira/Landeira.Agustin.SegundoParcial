2d8a4caf5ce7d201c866c91735933922d805aa64
agus2023 <landeira2017@gmail.com>
2023-11-08 15:10:44 -0300

    proyecto de pruebas hecho

--- pruebas de testeos/UnitTest1.cs
+++ pruebas de testeos/UnitTest1.cs
@@ -8,12 +8,53 @@ namespace pruebas_de_testeos
         [TestMethod]
         public void VerificarIgualdadDeJugadores()
         {
+            
             JugadorDeBasket jugadorDeBasket = new JugadorDeBasket(500,"aro",34, "agustin", "landeira", 31, EDeporte.Basquet);
             JugadorDeFutbol jugadorDeFutbol = new JugadorDeFutbol(32,"delantero",21,"agustin","landeira",23,EDeporte.Futbol);
 
+            
             bool rta = jugadorDeBasket == jugadorDeFutbol;
 
+           
             Assert.IsTrue(rta);
         }
+
+        [TestMethod]
+        public void VerificarDesigualdadDeJugadores()
+        {
+            JugadorDeBasket jugadorDeBasket = new JugadorDeBasket(500, "aro", 34, "agustin", "landeira", 31, EDeporte.Basquet);
+            JugadorDeFutbol jugadorDeFutbol = new JugadorDeFutbol(32, "delantero", 21, "francis", "fernandez", 23, EDeporte.Futbol);
+
+            bool rta = jugadorDeFutbol == jugadorDeBasket;
+
+            Assert.IsFalse(rta);
+        }
+
+        [TestMethod]
+        public void VerificarPromedioCorrecto()
+        {
+            JugadorDeFutbol jugadorDeFutbol = new JugadorDeFutbol(32, "delantero", 21, "agustin", "landeira", 23, EDeporte.Futbol);
+
+            
+            string promedio = jugadorDeFutbol.CalcularPromedio();
+            float resultado = (float)jugadorDeFutbol.Goles / jugadorDeFutbol.partidosJugados;
+            string promedioEsperado = resultado.ToString("0.00");
+
+            Assert.AreEqual(promedioEsperado,promedio);
+        }
+
+        [TestMethod]
+        public void VerificarListaNoNula()
+        {
+            JugadorDeBeisbol beisbolista = new JugadorDeBeisbol("bate",23,34,"barry","alen",21,EDeporte.Beisbol);
+            Registro miRegistro = new Registro();
+
+            
+            List<Jugadores> lista = miRegistro.ListaJugadores;
+
+            Assert.IsNotNull(lista);
+
+        }
+
     }
 }
