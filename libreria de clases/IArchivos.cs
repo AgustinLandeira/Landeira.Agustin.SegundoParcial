265f9fa5c1659e553e821efdadf4a98f41df41fa
agus2023 <landeira2017@gmail.com>
2023-11-10 11:01:34 -0300

    creando interfaz generica

---
+++ libreria de clases/IArchivos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libreria_de_clases
+{
+    public interface IArchivos<T>
+    {
+        bool VerificarDatos();
+        T Deserializar();
+    }
+}
